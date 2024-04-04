0a80545a100e09d5dfd8fbc9886c607e22f5bce6
davidfer1112 <davidfernando1112@gmail.com>
2024-04-04 17:48:32 -0500

    Feature: Header (Responsive)

--- src/components/Header-component/heder-component.tsx
+++ src/components/Header-component/heder-component.tsx
@@ -1,12 +1,21 @@
-
+import { useState } from "react"
 import Logo from "../../assets/images/Logo.svg"
 import carrito from "../../assets/images/carrito.svg" 
 import User from "../../assets/images/User.svg"
+import Menu from "../../assets/images/Menu.svg"
+import Back from "../../assets/images/Back.svg"
 import "./header-component.css"
 
 
-const Header = () => (
-    <header className="header">
+
+const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    return(
+<header className="header">
         <img src={Logo} alt="Logo Mi Portal"  className="logo"/>
             
         
@@ -22,9 +31,26 @@ const Header = () => (
             <button className="">
                 <img src={carrito} alt="carrito" />
             </button>
+            <button onClick={toggleMenu} className="menu-hamburguesa">
+                <img src={Menu} alt="menu" />
+            </button>
+        
+      </div>
+      
+      {isMenuOpen && (
+        <div className="overlay">
+            <button onClick={toggleMenu} className="close-menu">
+                <img src={Back} alt ="back" />
+            </button>
+                <ul>
+                    <li><a href="/">Productos</a></li>
+                    <li><a href="/">About</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
         </div>
+      )}
     </header>
-  )
-  
-  export default Header;
-  
+  );
+}
+
+export default Header;
